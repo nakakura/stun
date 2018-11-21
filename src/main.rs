1d50e06260c7ce2f7bfb383d0a336c32f4bea972
Toshiya Nakakura <nakakura@me.com>
2018-11-21 16:04:43 +0900

    close #1

--- src/main.rs
+++ src/main.rs
@@ -29,132 +29,94 @@ lazy_static! {
 
 const MAX_DATAGRAM_SIZE: usize = 1500;
 
-fn task1(socket: UdpSocket, remote_addr: &SocketAddr) -> UdpSocket {
-    let mut runtime = Runtime::new().unwrap();
+fn send_change_request(socket_addr: &SocketAddr, remote_addr: &SocketAddr, change_ip_flag: bool, change_port_flag: bool) -> Result<SocketAddr, ()> {
+    let socket = UdpSocket::bind(socket_addr).unwrap();
     let when = Instant::now() + Duration::from_millis(100);
+    let mut runtime = Runtime::new().unwrap();
 
     let task = Delay::new(when)
         .map_err(|e| panic!("timer failed; err={:?}", e))
         .and_then(|_| {
-            let change_request_internal = message::attributes::ChangeRequest::new(false, false);
+            let change_request_internal = message::attributes::ChangeRequest::new(change_ip_flag, change_port_flag);
             let change_request = message::Attribute::ChangeRequest(change_request_internal);
             let message = message::StunMessage::create_from_attributes(1, vec!(change_request));
             socket.send_dgram(message.encode().unwrap(), remote_addr)
         })
         .and_then(|(socket, _)| {
             socket.recv_dgram(vec![0u8; MAX_DATAGRAM_SIZE])
-        })
-        .and_then(|(socket, data, _len, _send_from)| {
-            let local_addr: SocketAddr = socket.local_addr().unwrap();
+        }).map_err(|_| ())
+        .and_then(|(_socket, data, _len, _send_from)| {
             let message = message::StunMessage::decode(&data).unwrap();
             if let message::Attribute::MappedAddress(ref mapped) = message.attributes[0] {
-                {
-                    if let (Some(ref addr), Some(ref port)) = (*(NAT_ADDR.lock().unwrap()), *(NAT_PORT.lock().unwrap())) {
-                        if addr != &mapped.address || port != &mapped.port {
-                            println!("synmetric");
-                            ::std::process::exit(0);
-                        }
-                    }
-                }
-                *(NAT_ADDR.lock().unwrap()) = Some(mapped.address);
-                *(NAT_PORT.lock().unwrap()) = Some(mapped.port);
-
-                // FIXME: local_addr.ip() returns 0.0.0.0. It's meaningless
-                if local_addr.ip() == mapped.address && local_addr.port() == mapped.port {
-                    println!("not behind nat");
-                    *(BEHIND_NAT.lock().unwrap()) = false;
-                } else {
-                    println!("behind nat");
-                    *(BEHIND_NAT.lock().unwrap()) = true;
-                }
+                let socket_addr = ::std::net::SocketAddr::new(IpAddr::V4(mapped.address), mapped.port);
+                Ok(socket_addr)
+            } else {
+                Err(())
             }
-            Ok(socket)
         });
 
-    match runtime.block_on(task) {
-        Ok(socket) => socket,
-        Err(e) => {
-            println!("{:?}", e);
-            ::std::process::exit(0);
+    runtime.block_on(task)
+}
+
+fn step1(socket_addr: &SocketAddr, remote_addr: &SocketAddr) {
+    let result = send_change_request(&socket_addr, &remote_addr, false, false);
+    if result.is_err() {
+        println!("UDP Blocked");
+        return;
+    }
+    let mapped_addr = result.unwrap();
+
+    step2(socket_addr, remote_addr, mapped_addr);
+}
+
+fn step2(socket_addr: &SocketAddr, remote_addr: &SocketAddr, mapped_addr: SocketAddr) {
+    let result = send_change_request(socket_addr, remote_addr, true, true);
+    if result.is_ok() {
+        if *socket_addr == mapped_addr {
+            println!("Open Internet");
+        } else {
+            println!("Full Cone");
+        }
+    } else {
+        if *socket_addr == mapped_addr {
+            println!("Symmetric UDP Firewall");
+        } else {
+            step3(socket_addr, remote_addr, mapped_addr);
         }
     }
 }
 
-fn task2(socket: UdpSocket, remote_addr: &SocketAddr) -> Result<UdpSocket, ::std::io::Error> {
-    let mut runtime = Runtime::new().unwrap();
-    let when = Instant::now() + Duration::from_millis(100);
-
-    let task = Delay::new(when)
-        .map_err(|e| panic!("timer failed; err={:?}", e))
-        .and_then(|_| {
-            let change_request_internal = message::attributes::ChangeRequest::new(true, true);
-            let change_request = message::Attribute::ChangeRequest(change_request_internal);
-            let message = message::StunMessage::create_from_attributes(1, vec!(change_request));
-            socket.send_dgram(message.encode().unwrap(), remote_addr)
-        })
-        .and_then(|(socket, _)| {
-            socket.recv_dgram(vec![0u8; MAX_DATAGRAM_SIZE])
-        })
-        .and_then(|(socket, data, _len, _send_from)| {
-            let local_addr: SocketAddr = socket.local_addr().unwrap();
-            let message = message::StunMessage::decode(&data).unwrap();
-            if let message::Attribute::MappedAddress(ref mapped) = message.attributes[0] {
-                *(NAT_ADDR.lock().unwrap()) = Some(mapped.address);
-                *(NAT_PORT.lock().unwrap()) = Some(mapped.port);
-
-                if *(BEHIND_NAT.lock().unwrap()) {
-                    println!("full corn");
-                } else {
-                    println!("direct connection to internet");
-                }
-            }
-            Ok(socket)
-        });
-
-    runtime.block_on(task)
-}
-
-fn task3(socket: UdpSocket, remote_addr: &SocketAddr) -> Result<UdpSocket, ::std::io::Error> {
-    let mut runtime = Runtime::new().unwrap();
-    let when = Instant::now() + Duration::from_millis(100);
-
-    let task = Delay::new(when)
-        .map_err(|e| panic!("timer failed; err={:?}", e))
-        .and_then(|_| {
-            let change_request_internal = message::attributes::ChangeRequest::new(true, true);
-            let change_request = message::Attribute::ChangeRequest(change_request_internal);
-            let message = message::StunMessage::create_from_attributes(1, vec!(change_request));
-            socket.send_dgram(message.encode().unwrap(), remote_addr)
-        })
-        .and_then(|(socket, _)| {
-            socket.recv_dgram(vec![0u8; MAX_DATAGRAM_SIZE])
-        })
-        .and_then(|(socket, data, _len, _send_from)| {
-            let local_addr: SocketAddr = socket.local_addr().unwrap();
-            let message = message::StunMessage::decode(&data).unwrap();
-            if let message::Attribute::MappedAddress(ref mapped) = message.attributes[0] {
-                println!("restricted");
-                ::std::process::exit(1);
-            }
-            Ok(socket)
-        });
-
-    runtime.block_on(task)
+fn step3(socket_addr: &SocketAddr, remote_addr: &SocketAddr, mapped_addr: SocketAddr) {
+    let mapped_addr_2 = send_change_request(socket_addr, remote_addr, false, false).unwrap();
+    if mapped_addr != mapped_addr_2 {
+        println!("Symmetric NAT")
+    } else {
+        let result = send_change_request(socket_addr, remote_addr, false, true);
+        if result.is_ok() {
+            println!("Restricted NAT");
+        } else {
+            println!("Port Restricted NAT");
+        }
+    }
 }
 
 fn main() {
+    let remote_addrs: Vec<_> = "stun.l.google.com:19302".to_socket_addrs()
+        .expect("Unable to resolve domain")
+        .filter(|x| x.is_ipv4())
+        .collect();
     let local_socket_addr = (|| {
         println!("please specify nic with id");
-        let ifaces = datalink::interfaces();
+        let ifaces: Vec<_> = datalink::interfaces().iter().flat_map(|x| x.ips.clone()).filter(|x| x.is_ipv4()).collect();
         for (id, iface) in (&ifaces).iter().enumerate() {
-            println!("{}: {:?}", id, iface.ips);
+            println!("{}: {:?}", id, iface);
         }
 
         let mut input = String::new();
         let ip_addr_opt = match ::std::io::stdin().read_line(&mut input) {
-            Ok(n) => {
+            Ok(_) => {
                 let id = input.trim().parse::<usize>().expect("input number");
-                ifaces.get(id)?.ips.iter().find(|x| x.is_ipv4()).map(|x| x.ip())
+                Some(ifaces.get(id)?.ip())
             },
             Err(error) => {
                 println!("error: {}", error);
@@ -168,54 +130,5 @@ fn main() {
         })
     })().expect("Please Select valid NIC");
 
-    let server_details = "stun.l.google.com:19302";
-    let remote_addr: Vec<_> = server_details.to_socket_addrs()
-        .expect("Unable to resolve domain")
-        .collect();
-    let local_addr: SocketAddr = "0.0.0.0:0".parse().unwrap();
-    let port = local_addr.port();
-    let socket = UdpSocket::bind(&local_addr).unwrap();
-    let socket = task1(socket, &remote_addr[0]);
-    let result = task2(socket, &remote_addr[0]);
-    result.map_err(|_| {
-        let addr: SocketAddr = format!("0.0.0.0:{}", port).parse().unwrap();
-        let socket = UdpSocket::bind(&local_addr).unwrap();
-        let socket = task1(socket, &remote_addr[0]);
-        let result = task3(socket, &remote_addr[0]);
-        result.map_err(|_| println!("port restricted") )
-    });
-
-
-
-/*
-        let change_request_internal = message::attributes::ChangeRequest::new(true, false);
-        let change_request = message::Attribute::ChangeRequest(change_request_internal);
-        let message = message::StunMessage::create_from_attributes(1, vec!(change_request));
-
-        socket.send_dgram(message.encode().unwrap(), &remote_addr[0])
-        */
-
-
-
-
-
-    /*
-    let processing = socket
-        .send_dgram(message.encode().unwrap(), &remote_addr[0])
-
-
-        .and_then(|(socket, _)| socket.recv_dgram(vec![0u8; MAX_DATAGRAM_SIZE]))
-        .map(|(socket, data, len, _)| {
-
-        })
-        .wait();
-    match processing {
-        Ok(_) => {}
-        Err(e) => eprintln!("Encountered an error: {}", e),
-    }
-
-*/
-
-
+    step1(&local_socket_addr, &remote_addrs[0]);
 }
-
